fd5a531471317db7819bbb90988fdd299cd6bd54
Vivek Palaniappan <vivekpalaniappan69@gmail.com>
2019-08-03 17:00:44 +0800

    edited path

--- run.py
+++ run.py
@@ -10,15 +10,15 @@ from data_processor.base_bars import BaseBars
 
 
 print('Creating tick bars...')
-base = BaseBars("data/raw_data/price_vol.csv", "data/processed_data/price_bars/tick_bars.csv", "tick", 10)
+base = BaseBars("sample_data/raw_data/price_vol.csv", "sample_data/processed_data/price_bars/tick_bars.csv", "tick", 10)
 base.batch_run()
 
 print('Creating dollar bars...')
-base = BaseBars("data/raw_data/price_vol.csv", "data/processed_data/price_bars/dollar_bars.csv", "dollar", 20000)
+base = BaseBars("sample_data/raw_data/price_vol.csv", "sample_data/processed_data/price_bars/dollar_bars.csv", "dollar", 20000)
 base.batch_run()
 
 print('Creating volume bars...')
-base = BaseBars("data/raw_data/price_vol.csv", "data/processed_data/price_bars/volume_bars.csv", "volume", 50)
+base = BaseBars("sample_data/raw_data/price_vol.csv", "sample_data/processed_data/price_bars/volume_bars.csv", "volume", 50)
 base.batch_run()
 
 print('Processing data...')
